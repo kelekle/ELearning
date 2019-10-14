4898a34e95b0de2ba40d362a7b7e13a36262d1cf
kelekle <1524183302@qq.com>
2019-10-14 17:19:53 +0800

    add screenshot

--- app/src/main/java/com/star/e_learning/activity/CourseContentActivity.java
+++ app/src/main/java/com/star/e_learning/activity/CourseContentActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.bumptech.glide.Glide;
 import com.star.e_learning.R;
 
 import fm.jiecao.jcvideoplayer_lib.JCVideoPlayerStandard;
@@ -17,14 +18,19 @@ public class CourseContentActivity extends AppCompatActivity implements View.OnC
     private Button syllabus;
 
     private JCVideoPlayerStandard jcVideoPlayerStandard;
-    String url = "http://wsqncdn.miaopai.com/stream/ed5HCfnhovu3tyIQAiv60Q__.mp4?ssig=49b4de055518087c72b26cdad6a91ef3&time_stamp=1571028173054";
+    String url = "http://txycdn.miaopai.com/stream/ed5HCfnhovu3tyIQAiv60Q__.mp4?ssig=f9223170ca41d7cf19494e1e4ab54664&time_stamp=1571047207734";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_content);
         jcVideoPlayerStandard = findViewById(R.id.player_list_video);
-        jcVideoPlayerStandard.setUp(url, jcVideoPlayerStandard.SCREEN_LAYOUT_NORMAL,"学英语");
+        boolean setup = jcVideoPlayerStandard.setUp(url, jcVideoPlayerStandard.SCREEN_LAYOUT_NORMAL,"学英语");
+        if(setup){
+            Glide.with(getApplicationContext())
+                    .load("http://a4.att.hudong.com/05/71/01300000057455120185716259013.jpg")
+                    .into(jcVideoPlayerStandard.thumbImageView);
+        }
         syllabus = findViewById(R.id.syllabus_btn);
         follow = findViewById(R.id.follow_btn);
         syllabus.setOnClickListener(this);
